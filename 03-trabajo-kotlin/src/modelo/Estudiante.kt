b095b28d82b115288bd3ce18c78877ca96b89585
Fercho <carferchomen@gmail.com>
2019-05-08 23:43:46 -0500

    some changes

---
+++ 03-trabajo-kotlin/src/modelo/Estudiante.kt
@@ -0,0 +1,11 @@
+package modelo
+
+open class Estudiante(
+    val identificación:Int,
+    val nombre:String,
+    val apellido:String,
+    val tipo_id:String,
+    val carrera:String,
+    val nivel:Int){
+
+}
