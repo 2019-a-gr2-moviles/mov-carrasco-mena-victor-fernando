07c4d860866032870711fb430ea858ea2692622c
HCfercho <carferchomen@gmail.com>
2019-05-16 15:53:12 -0500

    fun created

--- 02android/app/src/main/java/com/example/a02_android/MainActivity.kt
+++ 02android/app/src/main/java/com/example/a02_android/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.a02_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    fun irAActividadDos(){
+
+        val intent = Intent(
+            this,
+            Actividad_Dos::class.java
+        )
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_actividad_dos.setOnClickListener{
+            irAActividadDos()
+        }
     }
 }
