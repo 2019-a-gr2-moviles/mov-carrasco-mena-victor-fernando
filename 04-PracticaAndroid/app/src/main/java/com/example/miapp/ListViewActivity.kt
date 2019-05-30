8f617634c85f7c288aba3d9fd2290bc9416711da
Johanna Cerezo <caro.cerezor@gmail.com>
2019-05-30 15:55:28 -0500

    add list view

--- 04-PracticaAndroid/app/src/main/java/com/example/miapp/ListViewActivity.kt
+++ 04-PracticaAndroid/app/src/main/java/com/example/miapp/ListViewActivity.kt
@@ -1,7 +1,11 @@
 package com.example.miapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.util.Log
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_list_view.*
 
@@ -24,8 +28,23 @@ class ListViewActivity : AppCompatActivity() {
 
         lv_ejemplo.adapter = adapter
 
+        lv_ejemplo.onItemClickListener = AdapterView.OnItemClickListener {
+                parent, view, position, id ->
+            Log.i("list-view","Posicion $position")
+            Snackbar.make(view, "Posicion $position", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            //irAToast()
+
+        }
 
 
+    }
 
+    fun irAToast(){
+        val intentExplicito = Intent(
+            this,
+            Main2Activity::class.java
+        )
+        startActivity(intentExplicito)
     }
 }
