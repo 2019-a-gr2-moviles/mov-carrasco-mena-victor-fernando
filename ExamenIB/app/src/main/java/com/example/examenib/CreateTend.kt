b5de6d125e4e27711f37434c76b28de4093c0acf
VICTOR FERNANDO CARRASCO MENA <carferchomen@gmail.com>
2019-06-02 01:31:17 -0500

    activities and strings

---
+++ ExamenIB/app/src/main/java/com/example/examenib/CreateTend.kt
@@ -0,0 +1,12 @@
+package com.example.examenib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateTend : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_tend)
+    }
+}
