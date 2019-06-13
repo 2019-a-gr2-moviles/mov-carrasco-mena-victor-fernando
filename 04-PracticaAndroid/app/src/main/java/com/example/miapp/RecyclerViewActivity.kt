53e022a191c5474651eac78ae0dd328dcefbd8de
HCfercho <carferchomen@gmail.com>
2019-06-13 15:56:48 -0500

    nueva clase sobre intents

--- 04-PracticaAndroid/app/src/main/java/com/example/miapp/RecyclerViewActivity.kt
+++ 04-PracticaAndroid/app/src/main/java/com/example/miapp/RecyclerViewActivity.kt
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.myapplication.AdaptadorPersona
 import kotlinx.android.synthetic.main.activity_parcelable.*
+import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
 
@@ -13,22 +15,37 @@ class RecyclerViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
-    val lista =    arrayListOf<Persona>();
+        val lista =    arrayListOf<Persona>();
+        //val recycler_view = rv_Persona;
+        //val actividad = this;
 
-        val recycler_view = rv_Persona;
-        val actividad = this;
         lista.add(Persona("Fernando","171871553"))
         lista.add(Persona("Fernando1","171871554"))
         lista.add(Persona("Fernando2","171871553"))
 
-        val  adaptadorPersona=AdaptadorPersona(lista, actividad, recycler_view);
+        iniciarRecylerView(lista, this, rv_Persona)
 
-        rv_Persona.adapter = adaptadorPersona
-        rv_Persona.itemAnimator=DefaultItemAnimator()
-        rv_Persona.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun iniciarRecylerView(
+        lista: List<Persona>,
+        actividad: RecyclerViewActivity,
+        recycler_view: RecyclerView
+    ) {
+        val adaptadorPersona = AdaptadorPersona(
+            lista,
+            actividad,
+            recycler_view
+        )
+        recycler_view.adapter = adaptadorPersona
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        recycler_view.layoutManager = LinearLayoutManager(actividad)
 
         adaptadorPersona.notifyDataSetChanged()
     }
 
+    fun cambiarNombreTextView(texto: String) {
+        txt_titulo_rv.text = texto
+    }
 
 }
