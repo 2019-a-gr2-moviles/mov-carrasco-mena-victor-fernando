b7155be13dde1dfc1cee893450ba92464e6f39f6
HCfercho <carferchomen@gmail.com>
2019-07-18 14:40:06 -0500

    cilco vida activity

---
+++ 04-PracticaAndroid/app/src/main/java/com/example/miapp/CicloVidaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.miapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CicloVidaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciclo_vida)
+    }
+}
